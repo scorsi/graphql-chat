41430d9211d07e000061e27299821ae61bf5c8ed
scorsi <sylvain.corsini@gmail.com>
2018-09-21 12:00:53 +0200

    Add Mongoose to connect to Docker MongoDB.

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const {ApolloServer, gql} = require('apollo-server-express');
+import express from 'express';
+import {ApolloServer, gql} from 'apollo-server-express';
+import mongoose from "mongoose";
 
 const typeDefs = gql`
-    type Query {
-        hello: String
-    }
+  type Query {
+    hello: String
+  }
 `;
 
 const resolvers = {
@@ -18,6 +19,15 @@ const server = new ApolloServer({typeDefs, resolvers});
 const app = express();
 server.applyMiddleware({app});
 
-app.listen({port: 4000}, () =>
-  console.log(`🚀 Server ready at http://localhost:4000`),
+app.listen({port: 4000}, () => {
+    console.log(`✅  Server ready at http://localhost:4000`);
+    console.log(`➡️  GraphQL at http://localhost:4000/graphql`);
+    console.log(`➡️  Subscription at http://localhost:4000/ws`);
+
+    mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/main')
+      .then(() => {
+        console.log("✅  Successfully connected to MongoDB");
+      });
+    mongoose.connection.on('error', console.error.bind(console, '🔴  MongoDB connection error:'));
+  }
 );
