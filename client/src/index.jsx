096bbfdaa8ca53132ad96459508b9b38830830af
scorsi <sylvain.corsini@gmail.com>
2018-09-21 09:48:59 +0200

    Add eslint and remove TypeScript

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import Root from './components/Root';
+
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+,
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
